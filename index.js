090ad34d430b9158f97ab10debff503185596b57
Mauro Parafati <mparafati@fi.uba.ar>
2021-07-09 20:03:29 -0300

    Add utils

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const dbService = require('./src/services/dbService');
+const dbUtils = require('./src/utils/dbUtils');
 const knex = require('./src/utils/knex');
 
 module.exports = {
 	dbService,
+	dbUtils,
 	knex,
 };
---
+++ src/utils/dbUtils.js
@@ -0,0 +1,27 @@
+const { snakeCase: toSnakeCase } = require('snake-case');
+const toCamelCase = require('camelcase');
+
+module.exports = function dbUtils() {
+	return {
+		mapToDb,
+		mapFromDb,
+	};
+
+	function mapToDb(value) {
+		return mapDb(value, toSnakeCase);
+	}
+
+	function mapFromDb(value) {
+		return mapDb(value, toCamelCase);
+	}
+
+	// Private
+
+	function mapDb(value, func) {
+		if (_.isArray(value)) return value.map((val) => mapDb(val, func));
+
+		if (_.isObject(value)) return _.mapKeys(value, (val, key) => func(key));
+
+		return func(value);
+	}
+};
